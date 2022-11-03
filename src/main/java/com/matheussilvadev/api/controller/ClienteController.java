fd355f9e98a6629b4bce1f20d1c9cee5fe44088b
Matheus <matheuspsilva29@gmail.com>
2022-11-03 18:18:59 -0300

    Adição JPA

--- src/main/java/com/matheussilvadev/api/controller/ClienteController.java
+++ src/main/java/com/matheussilvadev/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.matheussilvadev.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,13 @@ import com.matheussilvadev.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente = new Cliente();
-		cliente.setId(1L);
-		cliente.setNome("Joao1");
-		cliente.setTelefone("999999999");
-		cliente.setEmail("joao@email.com");
 		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("989999999");
-		cliente2.setEmail("maria@email.com");
-		
-		return Arrays.asList(cliente, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 }
