712d042dd31fd16506e1553dc56574858b6f1f3a
Matheus <matheuspsilva29@gmail.com>
2022-11-03 20:18:46 -0300

    Adição do cliente repository e realizando DI por construtor

--- src/main/java/com/matheussilvadev/api/controller/ClienteController.java
+++ src/main/java/com/matheussilvadev/api/controller/ClienteController.java
@@ -5,21 +5,26 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheussilvadev.domain.model.Cliente;
+import com.matheussilvadev.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
-	
-	@PersistenceContext
-	private EntityManager manager;
+
+	//Não é necessário usar autowired pois é feita injeção de dependência pelo método construtor
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 	}
 
 }
